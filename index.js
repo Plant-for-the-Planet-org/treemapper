c3e4951b78ae563dec81a36edf21142ff701c85d
haideralishah <headeralishah@gmail.com>
2020-06-01 14:19:06 +0500

    add jail-monkey & stable

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import { name as appName } from './app.json';
 console.disableYellowBox = true;
 import Camera from './app/Components/CreatePolygon/Test'
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Camera);
