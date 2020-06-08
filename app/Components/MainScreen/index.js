577664ddb0e2c52a11c69eb998ac5ce8922ab772
haideralishah <headeralishah@gmail.com>
2020-06-08 17:12:46 +0500

    Add Video & set on Learn button

--- app/Components/MainScreen/index.js
+++ app/Components/MainScreen/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import { View, StyleSheet, ScrollView, Image, Text, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, ScrollView, Image, Text, ImageBackground, Modal, Dimensions } from 'react-native';
 import { PrimaryButton, LargeButton, Header } from '../Common';
 import { SafeAreaView } from 'react-native'
 import { Colors, Typography } from '_styles';
 import { MainScreenHeader } from '../Common/'
 import { main_screen_banner, map_texture } from '../../assets'
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Video from 'react-native-video';
 
+const { width, height } = Dimensions.get('window')
 const MainScreen = ({ navigation }) => {
+
+    const [isModalVisible, setIsModalVisible] = useState(false)
 
     let rightIcon = <Icon size={40} name={'play-circle'} color={'#707070'} />
 
     const onPressRegisterTree = () => navigation.navigate('TreeInventory')
+
+    const onPressLearn = () => setIsModalVisible(!isModalVisible)
+
+    const renderVideoModal = () => {
+        return (
+            <Modal visible={isModalVisible} animationType={'slide'}>
+                <View style={{ flex: 1, backgroundColor: '#000', padding: 30 }}>
+                    <Ionicons name={'md-close'} size={30} color={Colors.WHITE} onPress={onPressLearn} style={styles.closeIcon} />
+                    {isModalVisible && <Video
+                        repeat={true}
+                        resizeMode={'contain'}
+                        posterResizeMode={'stretch'}
+                        source={require('./learn.mp4')}
+                        style={styles.videoPLayer} />}
+                </View>
+            </Modal >
+        )
+    }
 
     return (
         <SafeAreaView style={styles.safeAreaViewCont}>
@@ -29,11 +52,12 @@ const MainScreen = ({ navigation }) => {
                         <LargeButton style={styles.customStyleLargeBtn} heading={'Download Maps'} active={false} subHeading={'for offline use'} />
                     </ImageBackground>
                     <ImageBackground source={map_texture} style={styles.bgImage}>
-                        <LargeButton rightIcon={rightIcon} style={styles.customStyleLargeBtn} heading={'Learn'} active={false} subHeading={'how to use Tree Mapper'} />
+                        <LargeButton onPress={onPressLearn} rightIcon={rightIcon} style={styles.customStyleLargeBtn} heading={'Learn'} active={false} subHeading={'how to use Tree Mapper'} />
                     </ImageBackground>
                 </ScrollView>
                 <PrimaryButton onPress={onPressRegisterTree} btnText={'Register Tree'} />
             </View>
+            {renderVideoModal()}
         </SafeAreaView>
     )
 }
@@ -66,6 +90,17 @@ const styles = StyleSheet.create({
     },
     bannerImage: {
         alignSelf: 'center'
+    },
+    videoPLayer: {
+        flex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
+    closeIcon: {
+        zIndex: 100
     }
 }
 )
