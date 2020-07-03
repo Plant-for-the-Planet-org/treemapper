4c1d26bc01dcbd3ef1c52e4c993e5278cc1d8d68
haideralishah <headeralishah@gmail.com>
2020-07-03 13:09:24 +0500

    React native auth0 ready to integrate

--- app/index.js
+++ app/index.js
@@ -1,14 +1,24 @@
 import 'react-native-gesture-handler';
 import React from 'react'
-import { SafeAreaView } from 'react-native'
-import Navigation from './navigation'
+import { SafeAreaView, Button } from 'react-native'
+import Auth0 from 'react-native-auth0';
+const auth0 = new Auth0({ domain: 'planetapp.eu.auth0.com', clientId: 'LiEqEef641Pzv8cBGn6i9Jt9jrnyLJEt' });
 
 const App = () => {
 
     return (
-        <SafeAreaView style={{flex:1}}>
-           <Navigation />
-        </SafeAreaView> 
+        <SafeAreaView style={{ flex: 1 }}>
+            <Button title={'AUTH 0 Mod'} onPress={() => {
+                auth0
+                    .webAuth
+                    .authorize({ scope: 'openid profile email' })
+                    .then(credentials => {
+                        console.log('credentials', credentials)
+                    }
+                    )
+                    .catch(error => console.log(error));
+            }}>AUTHO)))</Button>
+        </SafeAreaView>
     )
 }
 
