c2c61316a93209df669ca2a90d332512686e00eb
haideralishah <headeralishah@gmail.com>
2020-07-13 16:16:21 +0500

    Fix: svg images

--- app/assets/index.js
+++ app/assets/index.js
@@ -1,10 +1,8 @@
 // * PNGs
-import back_icon from './images/back_icon.png';
 import arrow_up from './images/arrow_up.png';
 import arrow_down from './images/arrow_down.png';
 import close from './images/close.png';
 import camera from './images/camera.png';
-import marker_png from './images/marker.png';
 import cloud from './images/cloud.png';
 import upload_check from './images/upload_check.png';
 import upload_now from './images/upload_now.png';
@@ -13,6 +11,7 @@ import map_texture from './images/map_texture.png';
 import placeholder_image from './images/placeholder_img.png';
 
 // * SVGs *
+import { marker_png } from './images/marker';
 import { main_screen_banner } from './images/main_screen_banner';
 import { two_trees } from './images/two_trees';
 import { plus_icon } from './images/plus_icon';
@@ -23,12 +22,11 @@ import { checkCircleFill } from "./images/check-circle-fill";
 import { active_marker } from './images/active_marker';
 
 export {
-    back_icon,
     arrow_up,
     arrow_down,
     close,
     camera,
-    marker_png,
+    marker_png ,
     active_marker,
     cloud,
     upload_check,
