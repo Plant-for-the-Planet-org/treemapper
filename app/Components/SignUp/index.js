c3dc8ae9f015c388f5a2d4fb7e9f4ad8c7d509b3
haideralishah <headeralishah@gmail.com>
2020-07-20 20:47:32 +0500

    Update: SignUp screen UI

--- app/Components/SignUp/index.js
+++ app/Components/SignUp/index.js
@@ -1,17 +1,11 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { View, StyleSheet, Text, ScrollView, Modal, Switch } from 'react-native';
-import { Header, LargeButton, PrimaryButton, Alrighty, Input } from '../Common';
+import React from 'react';
+import { View, StyleSheet, Text, ScrollView, Switch } from 'react-native';
+import { Header, PrimaryButton, Input } from '../Common';
 import { SafeAreaView } from 'react-native'
 import { Colors, Typography } from '_styles';
-import { addLocateTree, updateLastScreen } from '../../Actions'
-import { store } from '../../Actions/store';
-import JailMonkey from 'jail-monkey';
-import { color } from 'react-native-reanimated';
 
 
-const SignUp = ({ navigation }) => {
-
-
+const SignUp = ({ }) => {
     return (
         <SafeAreaView style={styles.mainContainer}>
             <View style={styles.container}>
@@ -22,65 +16,33 @@ const SignUp = ({ navigation }) => {
                         <Input label={'LAST NAME'} value={'Sanchez'} style={{ marginLeft: 15 }} />
                     </View>
                     <Input label={'EMAIL'} value={'startplanting@trees.com'} />
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 10 }}>
-                        <View style={{ height: 80, borderWidth: 2, flex: 1, borderRadius: 10, borderColor: Colors.PRIMARY, justifyContent: 'flex-end', marginRight: 5 }}>
-                            <Text style={{
-                                margin: 14, color: Colors.PRIMARY,
-                                fontFamily: Typography.FONT_FAMILY_BOLD,
-                                fontSize: Typography.FONT_SIZE_18,
-                            }}>Individual</Text>
+                    <View style={styles.selectRoleBtnsContainer}>
+                        <View style={[styles.roleBtnContainer, styles.marginRight]}>
+                            <Text style={styles.roleText}>Individual</Text>
                         </View>
-                        <View style={{ height: 80, borderWidth: 2, flex: 1, borderRadius: 10, borderColor: Colors.LIGHT_BORDER_COLOR, justifyContent: 'flex-end', marginLeft: 5 }}>
-                            <Text style={{
-                                margin: 14, color: Colors.LIGHT_BORDER_COLOR,
-                                fontFamily: Typography.FONT_FAMILY_BOLD,
-                                fontSize: Typography.FONT_SIZE_18,
-                            }}>Company</Text>
+                        <View style={[styles.roleBtnContainer, styles.marginLeft]}>
+                            <Text style={styles.roleText}>Company</Text>
                         </View>
                     </View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginVertical: 10 }}>
-                        <View style={{ height: 80, borderWidth: 2, flex: 1, borderRadius: 10, borderColor: Colors.PRIMARY, justifyContent: 'center', marginRight: 5, alignItems: 'center' }}>
-                            <Text style={{
-
-                                color: Colors.PRIMARY,
-                                fontFamily: Typography.FONT_FAMILY_BOLD,
-                                fontSize: Typography.FONT_SIZE_18,
-                            }}>Tree Planting Organisation</Text>
+                    <View style={styles.selectRoleBtnsContainer}>
+                        <View style={[[styles.roleBtnContainer, styles.activeRoleContainer], styles.justifyCenter, styles.marginRight]}>
+                            <Text style={[styles.roleText, styles.primaryText]}>Tree Planting Organisation</Text>
                         </View>
-                        <View style={{ height: 80, borderWidth: 2, flex: 1, borderRadius: 10, borderColor: Colors.LIGHT_BORDER_COLOR, justifyContent: 'flex-end', marginLeft: 5 }}>
-                            <Text style={{
-                                margin: 14, color: Colors.LIGHT_BORDER_COLOR,
-                                fontFamily: Typography.FONT_FAMILY_BOLD,
-                                fontSize: Typography.FONT_SIZE_18,
-                            }}>School</Text>
+                        <View style={[styles.roleBtnContainer, styles.marginLeft]}>
+                            <Text style={styles.roleText}>School</Text>
                         </View>
                     </View>
                     <Input label={'NAME OF TREE PLANTING ORGANIZATION'} value={'Forest in Africa'} />
-                    <View style={{ flexDirection: 'row', paddingVertical: 20 }}>
-                        <Text style={{
-                            flex: 1,
-                            fontFamily: Typography.FONT_FAMILY_REGULAR,
-                            fontSize: Typography.FONT_SIZE_16,
-                        }}>I agree to have my name published on the Plant-for-the-Planet App.</Text>
-                        <Switch
-                            trackColor={{ false: Colors.LIGHT_BORDER_COLOR, true: Colors.PRIMARY }}
-                            thumbColor={!false ? Colors.PRIMARY : Colors.WHITE}
-                            value={!false}
-                        />
+                    <View style={styles.switchContainer}>
+                        <Text style={styles.switchContainerText}>I agree to have my name published on the Plant-for-the-Planet App.</Text>
+                        <Switch trackColor={{ false: Colors.LIGHT_BORDER_COLOR, true: Colors.PRIMARY }} thumbColor={!false ? Colors.PRIMARY : Colors.WHITE} value={!false} />
                     </View>
-                    <View style={{ flexDirection: 'row', paddingVertical: 20 }}>
-                        <Text style={{
-                            flex: 1, fontFamily: Typography.FONT_FAMILY_REGULAR,
-                            fontSize: Typography.FONT_SIZE_16,
-                        }}>I agree that I may be contacted by Plant-for-the-Planet as a part of Tree Planting news and challenges.</Text>
-                        <Switch
-                            trackColor={{ false: Colors.LIGHT_BORDER_COLOR, true: Colors.PRIMARY }}
-                            thumbColor={!false ? Colors.PRIMARY : Colors.WHITE}
-                            value={!false}
-                        />
+                    <View style={styles.switchContainer}>
+                        <Text style={styles.switchContainerText}>I agree that I may be contacted by Plant-for-the-Planet as a part of Tree Planting news and challenges.</Text>
+                        <Switch trackColor={{ false: Colors.LIGHT_BORDER_COLOR, true: Colors.PRIMARY }} thumbColor={false ? Colors.PRIMARY : Colors.WHITE} value={false} />
                     </View>
                 </ScrollView>
-                <PrimaryButton btnText={'Continue'} />
+                <PrimaryButton btnText={'Create Profile'} />
             </View>
         </SafeAreaView>
     )
@@ -99,11 +61,34 @@ const styles = StyleSheet.create({
     cont: {
         flex: 1,
     },
-    addSpecies: {
-        color: Colors.ALERT,
+    selectRoleBtnsContainer: {
+        flexDirection: 'row', justifyContent: 'space-around', marginVertical: 10
+    },
+    roleBtnContainer: {
+        height: 80, borderWidth: 2, flex: 1, borderRadius: 10, borderColor: Colors.LIGHT_BORDER_COLOR, justifyContent: 'flex-end',
+    },
+    roleText: {
+        margin: 14, color: Colors.LIGHT_BORDER_COLOR,
         fontFamily: Typography.FONT_FAMILY_REGULAR,
         fontSize: Typography.FONT_SIZE_18,
-        lineHeight: Typography.LINE_HEIGHT_30,
-        textAlign: 'center'
-    }
+    },
+    switchContainerText: {
+        flex: 1,
+        fontFamily: Typography.FONT_FAMILY_REGULAR,
+        fontSize: Typography.FONT_SIZE_16,
+    },
+    switchContainer: {
+        flexDirection: 'row', paddingVertical: 20
+    },
+    primaryText: {
+        color: Colors.PRIMARY
+    },
+    justifyCenter: {
+        justifyContent: 'center'
+    },
+    activeRoleContainer: {
+        borderColor: Colors.PRIMARY
+    },
+    marginRight: { marginRight: 5 },
+    marginLeft: { marginLeft: 5 },
 })
