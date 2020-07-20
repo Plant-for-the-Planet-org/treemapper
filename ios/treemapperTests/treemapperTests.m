0fc5ca15b2a41b8e585068e797f7ce59d44fa81b
Sagar Aryal <sagar@aryal.me>
2020-07-20 20:37:41 +0200

    TreeMapper Name

--- ios/treemapperTests/treemapperTests.m
+++ ios/treemapperTests/treemapperTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface treeinventoryTests : XCTestCase
+@interface treemapperTests : XCTestCase
 
 @end
 
-@implementation treeinventoryTests
+@implementation treemapperTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
