52f6f8fdd19df037ab738b440145ec19694e7d80
Bright Amidiagbe <captainamiedi1@gmail.com>
2020-08-03 21:39:21 +0100

    Accessibility for automation first screen

--- app/Components/Common/Header/index.js
+++ app/Components/Common/Header/index.js
@@ -1,53 +1,77 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import { Colors, Typography } from '_styles';
-import { useNavigation } from '@react-navigation/native';
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {Colors, Typography} from '_styles';
+import {useNavigation} from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-
-const Header = ({ hideBackIcon, closeIcon, headingText, subHeadingText, onBackPress, textAlignStyle, style, subHeadingStyle }) => {
-    const navigation = useNavigation();
-    const onPressBack = () => onBackPress ? onBackPress() : navigation.goBack()
-    return (
-        <View style={style}>
-            <View style={styles.arrowContainer}>
-                {!hideBackIcon && <TouchableOpacity onPress={onPressBack} style={styles.paddingVertical}>
-                    <Ionicons name={closeIcon ? 'md-close' : 'md-arrow-back'} size={30} color={Colors.TEXT_COLOR} />
-                </TouchableOpacity>}
-                <View />
-            </View>
-            {headingText ? <View style={{ marginVertical: 0 }}>
-                <Text style={[styles.headerText, textAlignStyle]}>{headingText}</Text>
-            </View> : null}
-            {subHeadingText && <View style={{ marginVertical: 10, }}>
-                <Text style={[styles.subHeadingText, textAlignStyle, subHeadingStyle]}>{subHeadingText}</Text>
-            </View>}
+const Header = ({
+  hideBackIcon,
+  closeIcon,
+  headingText,
+  subHeadingText,
+  onBackPress,
+  textAlignStyle,
+  style,
+  subHeadingStyle,
+}) => {
+  const navigation = useNavigation();
+  const onPressBack = () => (onBackPress ? onBackPress() : navigation.goBack());
+  return (
+    <View style={style}>
+      <View style={styles.arrowContainer}>
+        {!hideBackIcon && (
+          <TouchableOpacity
+            onPress={onPressBack}
+            testID="header"
+            accessible={true}
+            accessibilityLabel="Header"
+            style={styles.paddingVertical}>
+            <Ionicons
+              name={closeIcon ? 'md-close' : 'md-arrow-back'}
+              size={30}
+              color={Colors.TEXT_COLOR}
+            />
+          </TouchableOpacity>
+        )}
+        <View />
+      </View>
+      {headingText ? (
+        <View style={{marginVertical: 0}}>
+          <Text style={[styles.headerText, textAlignStyle]}>{headingText}</Text>
         </View>
-    )
-}
-export default Header
+      ) : null}
+      {subHeadingText && (
+        <View style={{marginVertical: 10}}>
+          <Text
+            style={[styles.subHeadingText, textAlignStyle, subHeadingStyle]}>
+            {subHeadingText}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+};
+export default Header;
 
 const styles = StyleSheet.create({
-    headerText: {
-        fontFamily: Typography.FONT_FAMILY_EXTRA_BOLD,
-        fontSize: Typography.FONT_SIZE_27,
-        lineHeight: Typography.LINE_HEIGHT_40,
-        color: Colors.TEXT_COLOR,
-    },
-    subHeadingText: {
-        fontFamily: Typography.FONT_FAMILY_SEMI_BOLD,
-        fontSize: Typography.FONT_SIZE_20,
-        lineHeight: Typography.LINE_HEIGHT_24,
-        color: Colors.TEXT_COLOR,
-    },
-    backArrow: {
-
-    },
-    arrowContainer: {
-        flexDirection: 'row', justifyContent: 'space-between',
-    },
-    paddingVertical: {
-        paddingVertical: 15
-    }
-
-})
+  headerText: {
+    fontFamily: Typography.FONT_FAMILY_EXTRA_BOLD,
+    fontSize: Typography.FONT_SIZE_27,
+    lineHeight: Typography.LINE_HEIGHT_40,
+    color: Colors.TEXT_COLOR,
+  },
+  subHeadingText: {
+    fontFamily: Typography.FONT_FAMILY_SEMI_BOLD,
+    fontSize: Typography.FONT_SIZE_20,
+    lineHeight: Typography.LINE_HEIGHT_24,
+    color: Colors.TEXT_COLOR,
+  },
+  backArrow: {},
+  arrowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  paddingVertical: {
+    paddingVertical: 15,
+  },
+});
