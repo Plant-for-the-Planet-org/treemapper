6664bb488a6a0455a613e793a35cfc1449d00d8d
Tejas Sonar <tejassonar227@gmail.com>
2020-11-26 12:57:07 +0530

    upload feature

--- app/Actions/UploadInventory.js
+++ app/Actions/UploadInventory.js
@@ -101,7 +101,7 @@ const uploadInventory = () => {
                   });
               },
               (err) => {
-                alert(`${err}`);
+                //alert(`${err}`);
                 reject();
               }
             );
