23296baaec6c22e19c5d0bccc460f335ca571f6b
Ankit Gupta <agupta.ag19@gmail.com>
2021-01-11 13:47:05 +0530

    made image capturing code reusable

--- app/Components/Common/ImageCapturing/index.js
+++ app/Components/Common/ImageCapturing/index.js
@@ -1,6 +1,14 @@
 import React, { useState, useContext, useRef, useEffect } from 'react';
-import { View, StyleSheet, SafeAreaView, Image, TouchableOpacity, Modal } from 'react-native';
-import { Header, PrimaryButton, Alrighty } from '../Common';
+import {
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  Modal,
+  Platform,
+} from 'react-native';
+import { Header, PrimaryButton, Alrighty } from '../';
 import { Colors, Typography } from '_styles';
 import {
   insertImageAtIndexCoordinate,
@@ -8,13 +16,15 @@ import {
   getCoordByIndex,
   removeLastCoord,
   completePolygon,
-} from '../../Actions';
-import { store } from '../../Actions/store';
+  insertImageSingleRegisterTree,
+  getInventory,
+} from '../../../Actions';
+import { store } from '../../../Actions/store';
 import { useNavigation } from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { RNCamera } from 'react-native-camera';
 // import { APLHABETS } from '../../Utils/index';
-import { toLetters } from '../../Utils/mapMarkingCoordinate';
+import { toLetters } from '../../../Utils/mapMarkingCoordinate';
 import i18next from 'i18next';
 import RNFS from 'react-native-fs';
 
@@ -35,10 +45,11 @@ const infographicText = [
 
 const ImageCapturing = ({
   toggleState,
-  isCompletePolygon,
-  locationText,
-  activeMarkerIndex,
   updateActiveMarkerIndex,
+  activeMarkerIndex,
+  isCompletePolygon,
+  updateScreenState,
+  inventoryType,
 }) => {
   const camera = useRef();
 
@@ -46,18 +57,28 @@ const ImageCapturing = ({
   const { state } = useContext(store);
   const [imagePath, setImagePath] = useState('');
   const [APLHABETS, setAPLHABETS] = useState([]);
-
+  const [inventory, setInventory] = useState(null);
   const [isAlrightyModalShow, setIsAlrightyModalShow] = useState(false);
 
   useEffect(() => {
-    getCoordByIndex({ inventory_id: state.inventoryID, index: activeMarkerIndex }).then(
-      ({ coordsLength, coord }) => {
-        if (coord.imageUrl) {
-          setImagePath(coord.imageUrl);
+    if (inventoryType === 'multiple') {
+      getCoordByIndex({ inventory_id: state.inventoryID, index: activeMarkerIndex }).then(
+        ({ coordsLength, coord }) => {
+          if (coord.imageUrl) {
+            setImagePath(coord.imageUrl);
+          }
+        },
+      );
+      generateMarkers();
+    } else {
+      getInventory({ inventoryID: state.inventoryID }).then((inventory) => {
+        inventory.species = Object.values(inventory.species);
+        setInventory(inventory);
+        if (inventory.polygons[0]?.coordinates[0]?.imageUrl) {
+          setImagePath(inventory.polygons[0].coordinates[0].imageUrl);
         }
-      },
-    );
-    generateMarkers();
+      });
+    }
   }, []);
 
   const generateMarkers = () => {
@@ -67,6 +88,36 @@ const ImageCapturing = ({
       array.push(y);
     }
     setAPLHABETS(array);
+  };
+
+  const copyImageAndGetData = async () => {
+    // splits and stores the image path directories
+    let splittedPath = imagePath.split('/');
+    // splits and stores the file name and extension which is present on last index
+    let fileName = splittedPath.pop();
+    // splits and stores the file parent directory which is present on last index after pop
+    const inbox = splittedPath.pop();
+    // splits and stores the file extension
+    const fileExtension = fileName.split('.').pop();
+    // splits and stores the file name
+    fileName = fileName.split('.')[0];
+
+    // stores the destination path in which image should be stored
+    const outputPath = `${RNFS.DocumentDirectoryPath}/${fileName}.${fileExtension}`;
+
+    // stores the path from which the image should be copied
+    const inputPath = `${RNFS.CachesDirectoryPath}/${inbox}/${fileName}.${fileExtension}`;
+    try {
+      // copies the image to destination folder
+      await RNFS.copyFile(inputPath, outputPath);
+      let data = {
+        inventory_id: state.inventoryID,
+        imageUrl: Platform.OS === 'android' ? `file:///${outputPath}` : outputPath,
+      };
+      return data;
+    } catch (err) {
+      console.error('error while saving file', err);
+    }
   };
 
   const onPressCamera = async () => {
@@ -84,32 +135,11 @@ const ImageCapturing = ({
   };
 
   const onPressContinue = async () => {
-    if (isAlrightyModalShow) {
-      if (imagePath) {
-        // splits and stores the image path directories
-        let splittedPath = imagePath.split('/');
-        // splits and stores the file name and extension which is present on last index
-        let fileName = splittedPath.pop();
-        // splits and stores the file parent directory which is present on last index after pop
-        const inbox = splittedPath.pop();
-        // splits and stores the file extension
-        const fileExtension = fileName.split('.').pop();
-        // splits and stores the file name
-        fileName = fileName.split('.')[0];
-
-        // stores the destination path in which image should be stored
-        const outputPath = `${RNFS.DocumentDirectoryPath}/${fileName}.${fileExtension}`;
-
-        // stores the path from which the image should be copied
-        const inputPath = `${RNFS.CachesDirectoryPath}/${inbox}/${fileName}.${fileExtension}`;
-        try {
-          // copies the image to destination folder
-          await RNFS.copyFile(inputPath, outputPath);
-          let data = {
-            inventory_id: state.inventoryID,
-            imageUrl: imagePath,
-            index: activeMarkerIndex,
-          };
+    if (imagePath) {
+      try {
+        let data = await copyImageAndGetData();
+        if (inventoryType === 'multiple') {
+          data.index = activeMarkerIndex;
           insertImageAtIndexCoordinate(data).then(() => {
             if (isCompletePolygon) {
               setIsAlrightyModalShow(false);
@@ -119,25 +149,37 @@ const ImageCapturing = ({
               toggleState();
             }
           });
-        } catch (err) {
-          console.error('error while saving file', err);
+        } else {
+          insertImageSingleRegisterTree(data).then(() => {
+            navigation.navigate('SelectSpecies', {
+              species: inventory.species,
+              inventory: inventory,
+              visible: true,
+            });
+          });
         }
-      } else {
-        alert(i18next.t('label.image_capturing_required'));
+      } catch (err) {
+        console.error('error while saving file', err);
       }
     } else {
-      setIsAlrightyModalShow(true);
+      alert(i18next.t('label.image_capturing_required'));
     }
   };
 
   const onBackPress = () => {
-    removeLastCoord({ inventory_id: state.inventoryID }).then(() => {
-      toggleState();
-    });
+    if (inventoryType === 'multiple') {
+      removeLastCoord({ inventory_id: state.inventoryID }).then(() => {
+        toggleState();
+      });
+    } else {
+      updateScreenState('MapMarking');
+    }
   };
 
-  const onPressCompletePolygon = () => {
-    let data = { inventory_id: state.inventoryID, imageUrl: imagePath, index: activeMarkerIndex };
+  const onPressCompletePolygon = async () => {
+    let data = await copyImageAndGetData();
+    data.index = activeMarkerIndex;
+
     insertImageAtIndexCoordinate(data).then(() => {
       polygonUpdate({ inventory_id: state.inventoryID }).then(() => {
         completePolygon({ inventory_id: state.inventoryID }).then(() => {
@@ -172,7 +214,11 @@ const ImageCapturing = ({
       <View style={styles.screenMargin}>
         <Header
           onBackPress={onBackPress}
-          headingText={`Location ${APLHABETS[activeMarkerIndex]}`}
+          headingText={
+            inventoryType === 'multiple'
+              ? `Location ${APLHABETS[activeMarkerIndex]}`
+              : i18next.t('label.image_capturing_header')
+          }
           subHeadingText={i18next.t('label.image_capturing_sub_header')}
         />
       </View>
@@ -192,29 +238,45 @@ const ImageCapturing = ({
                   message: i18next.t('label.permission_camera_message'),
                   buttonPositive: i18next.t('label.permission_camera_ok'),
                   buttonNegative: i18next.t('label.permission_camera_cancel'),
-                }}></RNCamera>
+                }}
+              />
             </View>
           )}
         </View>
         <TouchableOpacity
           onPress={onPressCamera}
           style={styles.cameraIconContainer}
-          accessibilityLabel="Camera"
-          testID="camera_icon">
+          accessibilityLabel={inventoryType === 'multiple' ? 'Camera' : 'Register Tree Camera'}
+          testID={inventoryType === 'multiple' ? 'camera_icon' : 'register_tree_camera'}>
           <View style={styles.cameraIconCont}>
             <Ionicons name={imagePath ? 'md-camera-reverse' : 'md-camera'} size={25} />
           </View>
         </TouchableOpacity>
       </View>
-      <View style={styles.bottomBtnsContainer}>
+      <View
+        style={[
+          styles.bottomBtnsContainer,
+          inventoryType === 'single' ? { justifyContent: 'space-between' } : {},
+        ]}>
+        {inventoryType === 'single' && (
+          <PrimaryButton
+            onPress={onBackPress}
+            btnText={i18next.t('label.back')}
+            theme={'white'}
+            halfWidth
+          />
+        )}
         <PrimaryButton
           disabled={imagePath ? false : true}
-          onPress={onPressContinue}
+          onPress={
+            inventoryType === 'multiple' ? () => setIsAlrightyModalShow(true) : onPressContinue
+          }
           btnText={i18next.t('label.continue')}
-          style={styles.bottomBtnsWidth}
+          style={inventoryType === 'multiple' ? styles.bottomBtnsWidth : {}}
+          halfWidth={inventoryType === 'single'}
         />
       </View>
-      {renderAlrightyModal()}
+      {inventoryType === 'multiple' && renderAlrightyModal()}
     </SafeAreaView>
   );
 };
@@ -227,11 +289,6 @@ const styles = StyleSheet.create({
   screenMargin: {
     marginHorizontal: 25,
   },
-  cameraBelowTextContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginVertical: 20,
-  },
   bottomBtnsContainer: {
     flexDirection: 'row',
     marginHorizontal: 25,
@@ -242,12 +299,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.WHITE,
-  },
-  addSpecies: {
-    color: Colors.ALERT,
-    fontFamily: Typography.FONT_FAMILY_REGULAR,
-    fontSize: Typography.FONT_SIZE_18,
-    lineHeight: Typography.LINE_HEIGHT_30,
   },
   message: {
     color: Colors.TEXT_COLOR,
