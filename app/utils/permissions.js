b6b7ff68dd02cb4a2eb30806014c5d2e76ac415c
Tejas Sonar <tejassonar227@gmail.com>
2021-02-25 23:45:42 +0530

    linting

--- app/utils/permissions.js
+++ app/utils/permissions.js
@@ -3,29 +3,28 @@ import Geolocation from 'react-native-geolocation-service';
 
 const IS_ANDROID = Platform.OS === 'android';
 
-export const permission = async () => {
-  return new Promise(async (resolve, reject) => {
+export const permission = () => {
+  return new Promise((resolve, reject) => {
     if (IS_ANDROID) {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          // {
-          //   title: 'Location Permission',
-          //   message: 'App needs an access to your location',
-          //   buttonNegative: 'Cancel',
-          //   buttonPositive: 'OK',
-          // },
-        );
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log('You can use the location');
-          resolve();
-        } else {
-          console.log('Location permission denied');
-          reject();
-        }
-      } catch (err) {
-        console.warn(err);
-      }
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        // {
+        //   title: 'Location Permission',
+        //   message: 'App needs an access to your location',
+        //   buttonNegative: 'Cancel',
+        //   buttonPositive: 'OK',
+        // },
+      )
+        .then((granted) => {
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log('You can use the location');
+            resolve();
+          } else {
+            console.log('Location permission denied');
+            reject();
+          }
+        })
+        .catch((err) => console.warn(err));
     } else {
       Geolocation.requestAuthorization('whenInUse').then((permission) => {
         console.log(permission);
