98bbc2ffd7aa9aa2f92b0e1560e7738bcd9b8580
Tejas Sonar <tejassonar227@gmail.com>
2021-03-16 16:19:00 +0530

    linting

--- app/components/TreeInventory/index.js
+++ app/components/TreeInventory/index.js
@@ -214,8 +214,8 @@ const TreeInventory = ({ navigation }) => {
       {allInventory && allInventory.length > 0
         ? renderInventoryListContainer()
         : allInventory == null
-        ? renderLoadingInventoryList()
-        : renderEmptyInventoryList()}
+          ? renderLoadingInventoryList()
+          : renderEmptyInventoryList()}
       <PermissionBlockedAlert
         isPermissionBlockedAlertShow={isPermissionBlockedAlertShow}
         setIsPermissionBlockedAlertShow={setIsPermissionBlockedAlertShow}
