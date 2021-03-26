92c3ddfd73fbef587fbe9cc6dd8827cf802d1360
Tejas Sonar <tejassonar227@gmail.com>
2021-03-26 20:10:32 +0530

    disabled auto-upload

--- app/utils/checkLoginAndSync.js
+++ app/utils/checkLoginAndSync.js
@@ -7,12 +7,12 @@ export const checkLoginAndSync = async ({ sync, dispatch, userDispatch, connecte
   console.log(sync, internet, 'internet connection');
   const dbUserDetails = await getUserDetails();
   if (dbUserDetails && dbUserDetails.accessToken && sync && internet && connected) {
-    uploadInventoryData(dispatch, userDispatch);
+    // uploadInventoryData(dispatch, userDispatch);
   } else if (dbUserDetails && dbUserDetails.refreshToken && internet && connected) {
     const newAccessToken = await getNewAccessToken(dbUserDetails.refreshToken);
     if (newAccessToken) {
       if (sync) {
-        uploadInventoryData(dispatch, userDispatch);
+        // uploadInventoryData(dispatch, userDispatch);
       } else {
         // fetches the user details from server by passing the accessToken which is used while requesting the API
         getUserDetailsFromServer(newAccessToken);
