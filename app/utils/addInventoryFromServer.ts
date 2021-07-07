97bdec48dede45244c0bf4086ee742a916ef6fd6
Ankit Gupta <agupta.ag19@gmail.com>
2021-07-07 17:10:19 +0530

    fixed duplication of inventory when non synced species are present

--- app/utils/addInventoryFromServer.ts
+++ app/utils/addInventoryFromServer.ts
@@ -1,23 +1,25 @@
 import { getAllInventoryFromServer } from '../actions/inventory';
 import { addInventoryToDB, addSampleTree, getInventoryByStatus } from '../repositories/inventory';
+import { SYNCED } from './inventoryConstants';
 
 export const addInventoryFromServer = async () => {
-  let exceptSampleTrees;
-  let allSampleTrees;
+  let exceptSampleTrees: any;
+  let allSampleTrees: any;
   let allInventoryFromServer = await getAllInventoryFromServer();
   exceptSampleTrees = allInventoryFromServer[0];
   allSampleTrees = allInventoryFromServer[1];
-  getInventoryByStatus('all')
+  getInventoryByStatus(SYNCED)
     .then((allInventory) => {
       for (const inventoryFromServer of exceptSampleTrees) {
         if (allInventory.length === 0) {
           addInventoryToDB(inventoryFromServer);
-        }
-        for (const inventory of allInventory) {
-          if (inventoryFromServer.id === inventory.locationId) {
-            break;
-          } else if (inventory.locationId === allInventory[allInventory.length - 1].locationId) {
-            addInventoryToDB(inventoryFromServer);
+        } else {
+          for (const inventory of allInventory) {
+            if (inventoryFromServer.id === inventory.locationId) {
+              break;
+            } else if (inventory.locationId === allInventory[allInventory.length - 1].locationId) {
+              addInventoryToDB(inventoryFromServer);
+            }
           }
         }
       }
