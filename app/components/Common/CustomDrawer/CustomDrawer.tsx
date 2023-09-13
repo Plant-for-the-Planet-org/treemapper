457d3d26597bee92aa44b9571b38432ff16b4272
Mayank Kumawat <mayankkumawat015@gmail.com>
2023-09-13 18:27:05 +0530

    login in drawer

--- app/components/Common/CustomDrawer/CustomDrawer.tsx
+++ app/components/Common/CustomDrawer/CustomDrawer.tsx
@@ -1,7 +1,7 @@
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import React, { useEffect, useState, useContext } from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import { View, SafeAreaView, TouchableOpacity, StyleSheet, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
 
 import {
   LogoutSign,
@@ -11,10 +11,18 @@ import {
   single_tree_png,
   PieAdditionalData,
 } from '../../../assets';
-import { auth0Logout } from '../../../actions/user';
+import PrimaryButton from '../PrimaryButton';
+import { APIConfig } from '../../../actions/Config';
 import { Colors, Typography } from '../../../styles';
 import { UserContext } from '../../../reducers/user';
-import { getUserInformation } from '../../../repositories/user';
+import { LoadingContext } from '../../../reducers/loader';
+import { useDrawerStatus } from '@react-navigation/drawer';
+import { InventoryContext } from '../../../reducers/inventory';
+import { auth0Login, auth0Logout } from '../../../actions/user';
+import { startLoading, stopLoading } from '../../../actions/loader';
+import { getUserDetails, isLogin } from '../../../repositories/user';
+
+const { protocol, cdnUrl, webAppUrl } = APIConfig;
 
 const getIcon = screenName => {
   switch (screenName) {
@@ -52,18 +60,64 @@ const getLabel = screenName => {
 
 const CustomDrawer = props => {
   const { navigation, state } = props;
-  const [userInfo, setUserInfo] = useState(null);
+  const [userInfo, setUserInfo] = useState<any>({});
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const { dispatch: userDispatch } = useContext(UserContext);
+  const { dispatch } = useContext(InventoryContext);
+  const { state: loadingState, dispatch: loadingDispatch } = useContext(LoadingContext);
+
+  const isDrawerOpen = useDrawerStatus() === 'open';
 
   const handleLogout = async () => {
     await auth0Logout(userDispatch);
+    setIsLoggedIn(false);
   };
 
   useEffect(() => {
-    getUserInformation().then(data => {
-      setUserInfo(data);
+    isLogin().then(isUserLogin => {
+      setIsLoggedIn(isUserLogin);
     });
-  }, []);
+  }, [isDrawerOpen]);
+
+  useEffect(() => {
+    if (Object.keys(userInfo).length < 1) {
+      fetchUserDetails();
+    }
+  }, [isDrawerOpen]);
+
+  const fetchUserDetails = () => {
+    if (!loadingState.isLoading) {
+      getUserDetails().then(userDetails => {
+        if (userDetails) {
+          const stringifiedUserDetails = JSON.parse(JSON.stringify(userDetails));
+          if (stringifiedUserDetails) {
+            setUserInfo(stringifiedUserDetails);
+            setIsLoggedIn(!!stringifiedUserDetails.accessToken);
+          }
+        }
+      });
+    }
+  };
+
+  const onPressLogin = async () => {
+    startLoading()(loadingDispatch);
+    auth0Login(userDispatch, dispatch)
+      .then(() => {
+        stopLoading()(loadingDispatch);
+        fetchUserDetails();
+      })
+      .catch(err => {
+        if (err?.response?.status === 303) {
+          navigation.navigate('SignUp');
+        }
+        stopLoading()(loadingDispatch);
+      });
+  };
+
+  let avatar =
+    cdnUrl && userInfo.image
+      ? `${protocol}://${cdnUrl}/media/cache/profile/avatar/${userInfo.image}`
+      : '';
 
   return (
     <>
@@ -78,20 +132,31 @@ const CustomDrawer = props => {
             </TouchableOpacity>
             <View style={styles.profile}>
               <View style={styles.profileInfo}>
-                <Image resizeMode={'contain'} source={single_tree_png} style={styles.avatar} />
-                <View style={styles.profileInfoTextCon}>
-                  <Text style={styles.username}>
-                    {userInfo?.firstName + '' + userInfo?.lastName}
-                  </Text>
-                  <Text style={styles.email}>{userInfo?.email}</Text>
-                </View>
+                {avatar ? (
+                  <Image resizeMode={'contain'} source={{ uri: avatar }} style={styles.avatar} />
+                ) : (
+                  <Image resizeMode={'contain'} source={single_tree_png} style={styles.avatar} />
+                )}
+                {isLoggedIn ? (
+                  <View style={styles.profileInfoTextCon}>
+                    <Text style={styles.username}>
+                      {userInfo?.firstName + '' + userInfo?.lastName}
+                    </Text>
+                    <Text style={styles.email}>{userInfo?.email}</Text>
+                  </View>
+                ) : (
+                  <View style={styles.profileInfoTextCon}>
+                    <Text style={styles.username}>Guest User</Text>
+                  </View>
+                )}
               </View>
-              <TouchableOpacity activeOpacity={0.7} style={styles.pencil}>
-                <FontAwesome name="pencil" size={24} color={Colors.PRIMARY} />
-              </TouchableOpacity>
+              {isLoggedIn && (
+                <TouchableOpacity activeOpacity={0.7} style={styles.pencil}>
+                  <FontAwesome name="pencil" size={24} color={Colors.PRIMARY} />
+                </TouchableOpacity>
+              )}
             </View>
           </View>
-
           <View style={styles.drawerItemContainer}>
             {state.routeNames.map(
               (name, index) =>
@@ -115,14 +180,26 @@ const CustomDrawer = props => {
                   </TouchableOpacity>
                 ),
             )}
-            <TouchableOpacity activeOpacity={0.7} style={styles.drawerItem} onPress={handleLogout}>
-              <View style={styles.drawerItemInfo}>
-                {getIcon('Logout')}
-                <Text style={styles.drawerItemLabel}>{getLabel('Logout')}</Text>
-              </View>
-              <Ionicons name={'chevron-forward-outline'} size={20} color={Colors.TEXT_COLOR} />
-            </TouchableOpacity>
+            {isLoggedIn && (
+              <TouchableOpacity
+                activeOpacity={0.7}
+                style={styles.drawerItem}
+                onPress={handleLogout}>
+                <View style={styles.drawerItemInfo}>
+                  {getIcon('Logout')}
+                  <Text style={styles.drawerItemLabel}>{getLabel('Logout')}</Text>
+                </View>
+                <Ionicons name={'chevron-forward-outline'} size={20} color={Colors.TEXT_COLOR} />
+              </TouchableOpacity>
+            )}
           </View>
+          {!isLoggedIn && (
+            <PrimaryButton
+              onPress={onPressLogin}
+              btnText={'Login / Signup'}
+              style={styles.loginBtn}
+            />
+          )}
         </View>
       </SafeAreaView>
     </>
@@ -202,5 +279,11 @@ const styles = StyleSheet.create({
   drawerItemInfo: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  loginBtn: {
+    bottom: 0,
+    width: '90%',
+    alignSelf: 'center',
+    position: 'absolute',
   },
 });
