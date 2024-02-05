fe8fdb41d86d6b9448cba3fa345371f25c6859d6
Mayank Kumawat <mayankkumawat015@gmail.com>
2024-02-05 13:29:58 +0530

    added translation

--- app/components/Navigator/BottomTab.tsx
+++ app/components/Navigator/BottomTab.tsx
@@ -5,6 +5,7 @@ import MainScreen from '../MainScreen';
 import { ComingSoon } from '../Common';
 import BottomBar from '../MainScreen/BottomBar';
 import TreeInventory from '../TreeInventory';
+import i18next from 'i18next';
 
 const screenOptions = { headerShown: false };
 
@@ -18,14 +19,26 @@ const BottomTab = () => {
   const tabBar = (props: BottomTabBarProps) => <BottomBar {...props} />;
   return (
     <Tab.Navigator screenOptions={screenOptions} tabBar={tabBar}>
-      <Tab.Screen name="MainScreen" options={{ tabBarLabel: 'Map' }} component={MainScreen} />
+      <Tab.Screen
+        name="MainScreen"
+        options={{ tabBarLabel: i18next.t('label.map') }}
+        component={MainScreen}
+      />
       <Tab.Screen
         name="TreeInventory"
-        options={{ tabBarLabel: 'Tree Inventory' }}
+        options={{ tabBarLabel: i18next.t('label.tree_inventory') }}
         component={TreeInventory}
       />
-      <Tab.Screen name="Plots" component={ComingSoon} />
-      <Tab.Screen name="Add" component={AddComponent} />
+      <Tab.Screen
+        name="Plots"
+        options={{ tabBarLabel: i18next.t('label.plots') }}
+        component={ComingSoon}
+      />
+      <Tab.Screen
+        name="Add"
+        options={{ tabBarLabel: i18next.t('label.add') }}
+        component={AddComponent}
+      />
     </Tab.Navigator>
   );
 };
