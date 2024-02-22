d2c14c4ec49e625ae4496294803f9e55e7dbe868
shyambhongle <shyambhongle@gmail.com>
2024-02-22 05:42:27 +0530

    Fixed for screen stuck at "Loading content" at Single Tree Registration

--- app/components/RegisterSingleTree/index.tsx
+++ app/components/RegisterSingleTree/index.tsx
@@ -29,88 +29,113 @@ const RegisterSingleTree = () => {
 
   useEffect(() => {
     BackHandler.addEventListener('hardwareBackPress', hardBackHandler);
-    setTimeout(()=>{
-      setUpState()
-    },100);
+    setTimeout(setUpState,100);
     return () => {
       BackHandler.removeEventListener('hardwareBackPress', hardBackHandler);
     };
   }, [inventoryState, isGranted, navigation]);
 
-  const setUpState=()=>{
-    setScreenState('');
-    if (inventoryState.inventoryID) {
-      getInventory({ inventoryID: inventoryState.inventoryID }).then(InventoryData => {
-        if (InventoryData.status === INCOMPLETE) {
-          let data = {
-            inventory_id: inventoryState.inventoryID,
-            lastScreen: 'RegisterSingleTree',
-          };
-          updateLastScreen(data);
 
-          locationPermission()
-            .then(granted => {
-              setIsGranted(true);
-              if (granted && InventoryData.polygons[0]) {
-                Geolocation.getCurrentPosition(
-                  position => {
-                    const distanceInMeters = distanceCalculator(
-                      [position.coords.latitude, position.coords.longitude],
-                      [
-                        InventoryData.polygons[0].coordinates[0].latitude,
-                        InventoryData.polygons[0].coordinates[0].longitude,
-                      ],
-                      'meters',
-                    );
-                    if (distanceInMeters < 100) {
-                      //set onsite
-                      addLocateTree({
-                        inventory_id: inventoryState.inventoryID,
-                        locateTree: ON_SITE,
-                      });
-                      updateScreenState('ImageCapturing');
-                    } else {
-                      //set offsite
-                      addLocateTree({
-                        inventory_id: inventoryState.inventoryID,
-                        locateTree: OFF_SITE,
-                      });
-                      updateScreenState('MapMarking');
-                      navigation.navigate('SelectSpecies');
-                    }
-                  },
-                  (err: Geolocation.GeoError) => {
-                    console.error(err);
-                  },
-                  {
-                    enableHighAccuracy: true,
-                    timeout: 5000,
-                    maximumAge: 20000,
-                    accuracy: {
-                      android: 'high',
-                      ios: 'bestForNavigation',
-                    },
-                  },
-                );
-              } else {
-                setScreenState('MapMarking');
-              }
-            })
-            .catch(err => {
-              checkPermissionAlert(err);
-            });
-        }
-      });
-    } else {
-      locationPermission()
-        .then(() => {
-          setIsGranted(true);
-          setScreenState('MapMarking');
-        })
-        .catch(err => {
-          checkPermissionAlert(err);
-        });
+  const setUpState = async () => {
+    setScreenState('');
+    try {
+      if (inventoryState.inventoryID) {
+        await handleInventoryState();
+      } else {
+        await handleLocationPermission();
+      }
+    } catch (err) {
+      checkPermissionAlert(err);
     }
+  };
+  
+  const handleInventoryState = async () => {
+    const InventoryData = await getInventory({ inventoryID: inventoryState.inventoryID });
+    if (InventoryData.status === INCOMPLETE) {
+      await handleIncompleteInventory(InventoryData);
+    }else{
+      await handleLocationPermission();
+    }
+  };
+  
+  const handleLocationPermission = async () => {
+    await locationPermission();
+    setIsGranted(true);
+    setScreenState('MapMarking');
+  };
+  
+
+  const handleIncompleteInventory = async (InventoryData: { polygons: any[]; }) => {
+    let data = {
+      inventory_id: inventoryState.inventoryID,
+      lastScreen: 'RegisterSingleTree',
+    };
+    updateLastScreen(data);
+  
+    try {
+      const granted = await locationPermission();
+      setIsGranted(true);
+  
+      if (granted && InventoryData.polygons[0]) {
+        await handleGeolocation(InventoryData);
+      } else {
+        setScreenState('MapMarking');
+      }
+    } catch (err) {
+      checkPermissionAlert(err);
+    }
+  };
+  
+  const handleGeolocation = async (InventoryData: { polygons: { coordinates: { longitude: number; }[]; }[]; }) => {
+    try {
+      const position = await getCurrentPosition(); // Assuming this is a wrapper around Geolocation.getCurrentPosition
+      const distanceInMeters = distanceCalculator(
+        [position.coords.latitude, position.coords.longitude],
+        [
+          InventoryData.polygons[0].coordinates[0].latitude,
+          InventoryData.polygons[0].coordinates[0].longitude,
+        ],
+        'meters',
+      );
+  
+      if (distanceInMeters < 100) {
+        // Set onsite
+        addLocateTree({
+          inventory_id: inventoryState.inventoryID,
+          locateTree: ON_SITE,
+        });
+        updateScreenState('ImageCapturing');
+      } else {
+        // Set offsite
+        addLocateTree({
+          inventory_id: inventoryState.inventoryID,
+          locateTree: OFF_SITE,
+        });
+        updateScreenState('MapMarking');
+        navigation.navigate('SelectSpecies');
+      }
+    } catch (err) {
+      console.error(err);
+      // Handle geolocation error
+    }
+  };
+  
+  const getCurrentPosition = () => {
+    return new Promise((resolve, reject) => {
+      Geolocation.getCurrentPosition(
+        position => resolve(position),
+        error => reject(error),
+        {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 20000,
+          accuracy: {
+            android: 'high',
+            ios: 'bestForNavigation',
+          },
+        },
+      );
+    });
   };
 
 
