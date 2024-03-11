4d986d0f2f98b7d454385133bfb1a70627bc274a
shyambhongle <shyambhongle@gmail.com>
2024-03-11 16:44:43 +0530

    App theme and constant added

---
+++ src/utils/constants/common.ts
@@ -0,0 +1,38 @@
+import {TextStyle, ViewStyle} from 'react-native';
+import * as Colors from './colors';
+import * as Typography from './typography';
+
+// Font styles used in PFP
+
+// Used in the header in all the pages
+// use StyleSheet.create()
+
+export const bottomInputContainer: ViewStyle = {
+  flexDirection: 'row',
+  height: 65,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: Colors.WHITE,
+  borderTopWidth: 0.5,
+  borderColor: Colors.TEXT_COLOR,
+  paddingLeft: 20,
+  paddingRight: 10,
+};
+
+export const bottomInputLabel: TextStyle = {
+  fontFamily: Typography.FONT_FAMILY_REGULAR,
+  fontSize: Typography.FONT_SIZE_18,
+  lineHeight: Typography.LINE_HEIGHT_30,
+  color: Colors.TEXT_COLOR,
+  marginRight: 10,
+  paddingRight: 10,
+};
+
+export const bottomInputText: TextStyle = {
+  fontFamily: Typography.FONT_FAMILY_REGULAR,
+  fontSize: Typography.FONT_SIZE_20,
+  color: Colors.TEXT_COLOR,
+  fontWeight: Typography.FONT_WEIGHT_MEDIUM,
+  flex: 1,
+  paddingVertical: 10,
+};
