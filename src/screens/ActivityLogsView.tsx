93b83c6300a5bb60dbe11822cd8d9e47861cbf8a
shyambhongle <shyambhongle@gmail.com>
2024-05-09 14:01:57 +0530

    Share Activity logs utiliy added

--- src/screens/ActivityLogsView.tsx
+++ src/screens/ActivityLogsView.tsx
@@ -1,14 +1,59 @@
-import { SafeAreaView, StyleSheet} from 'react-native'
-import React from 'react'
+import { ActivityIndicator, SafeAreaView, StyleSheet, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import Header from 'src/components/common/Header'
 import ActivityLogsTab from 'src/components/activityLogs/ActivityLogsTab'
 import { Colors } from 'src/utils/constants'
+import { useRealm } from '@realm/react'
+import { View } from 'moti'
+import Share from 'react-native-share';
+import { RealmSchema } from 'src/types/enum/db.enum'
+import { toBase64 } from 'src/utils/constants/base64'
+import ShareIcon from 'assets/images/svg/ShareIcon.svg';
 
 const ActivityLogsView = () => {
+    const [loading, showLoading] = useState(false)
+    const realm = useRealm();
+
+    const getAllLogs = async () => {
+        showLoading(true)
+        const allLogs = realm
+            .objects(RealmSchema.ActivityLogs)
+        shareLogs(allLogs)
+    }
+
+    const shareLogs = async (logs: any) => {
+        const options = {
+            url: 'data:application/json;base64,' + toBase64(JSON.stringify(logs)),
+            message: "All user logs for TreeMapper app",
+            title: "Activity Logs",
+            filename: `TreeMapper GeoJSON`,
+            saveToFiles: true,
+        };
+        Share.open(options)
+            .then(() => {
+                showLoading(false)
+            })
+            .catch(err => {
+                showLoading(false)
+                // shows error if occurred and not canceled by the user
+                if (err?.error?.code != 'ECANCELLED500' && err?.message !== 'User did not share') {
+                    // iOS cancel button pressed
+                    console.log("iOS cancel button pressed")
+                }
+            });
+    }
+
+    const renderShareIcon = () => {
+        return (<TouchableOpacity onPress={getAllLogs} style={styles.rightContainer}>
+            <ShareIcon width={20} height={20} />
+        </TouchableOpacity>)
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <Header label="Activity Logs" />
+            <Header label="Activity Logs" rightComponet={renderShareIcon()} />
             <ActivityLogsTab />
+            {loading && <View style={styles.modal} ><ActivityIndicator size='small' color={Colors.NEW_PRIMARY}></ActivityIndicator></View>}
         </SafeAreaView>
     )
 }
@@ -18,6 +63,21 @@ export default ActivityLogsView
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Colors.WHITE
+        backgroundColor: Colors.WHITE,
+        paddingTop: 20
+    },
+    modal: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "rgba(0,0,0,0.5)",
+        zIndex: 1,
+        position: 'absolute'
+    },
+    rightContainer: {
+        width: 80,
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
