50cf2d4706a9309c826b1324bdf4a2f730a93a20
shyambhongle <shyambhongle@gmail.com>
2024-06-15 23:43:01 +0530

    Popover menu added

--- src/screens/PlotView.tsx
+++ src/screens/PlotView.tsx
@@ -1,16 +1,18 @@
-import { Pressable, StyleSheet, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from 'src/components/common/Header'
 import PlotList from 'src/components/monitoringPlot/PlotList'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Colors } from 'src/utils/constants'
+import { Colors, Typography } from 'src/utils/constants'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { RootStackParamList } from 'src/types/type/navigation.type'
-import AddIcon from 'assets/images/svg/Addicon.svg'
+import AddIcon from 'assets/images/svg/MoreOptionIcon.svg'
 import { useQuery } from '@realm/react'
 import { RealmSchema } from 'src/types/enum/db.enum'
 import { MonitoringPlot } from 'src/types/interface/slice.interface'
+import Popover from 'react-native-popover-view';
+
 
 const PlotView = () => {
 
@@ -22,13 +24,36 @@ const PlotView = () => {
   )
 
   const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
+
   const addGroups = () => {
     navigation.navigate('PlotGroup')
   }
 
 
+
+
   const renderIcon = () => {
-    return <Pressable onPress={addGroups} style={styles.rightContainer}><AddIcon width={16} height={16} /></Pressable>
+    return <Popover
+      backgroundStyle={{ opacity: 0 }}
+      popoverStyle={{
+        borderColor: Colors.GRAY_LIGHT,
+        shadowColor: Colors.GRAY_LIGHT,
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.6,
+        shadowRadius: 5,
+        elevation: 2,
+        borderRadius: 8,
+      }}
+      from={(
+        <Pressable onPress={addGroups} style={styles.rightContainer}><AddIcon width={16} height={16} /></Pressable>
+      )}>
+      <View style={styles.popOverWrapper}>
+        <Text style={styles.menuLabel}>Plot Group</Text>
+      </View>
+
+    </Popover>
+
+
   }
   return (
     <SafeAreaView style={styles.container}>
@@ -56,8 +81,19 @@ const styles = StyleSheet.create({
     height: 30,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: Colors.NEW_PRIMARY,
     marginRight: 20,
     borderRadius: 8
+  },
+  popOverWrapper: {
+    width: 100,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: "center",
+  },
+  menuLabel: {
+    fontSize: 14,
+    fontFamily: Typography.FONT_FAMILY_SEMI_BOLD,
+    color: Colors.TEXT_COLOR,
+
   }
 })
