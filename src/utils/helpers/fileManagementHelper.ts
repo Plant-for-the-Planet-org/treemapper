77f7f1a75031b7a05ea3db333531dd883882c9dd
shyambhongle <shyambhongle@gmail.com>
2024-06-23 21:29:58 +0530

    Logs:Folder creation minor

--- src/utils/helpers/fileManagementHelper.ts
+++ src/utils/helpers/fileManagementHelper.ts
@@ -3,18 +3,15 @@ import { InterventionData } from 'src/types/interface/slice.interface';
 import { zip } from 'react-native-zip-archive';
 import Share from 'react-native-share';
 
-const mainFolder = "TreeMapper"
-export const basePath = `${RNFS.DocumentDirectoryPath}/${mainFolder}`
-const tempDirectory = `${RNFS.CachesDirectoryPath}/TreeMapper_Data_${Date.now()}.zip`
+const mainFolder = "TreeMapper";
+export const basePath = `${RNFS.DocumentDirectoryPath}/${mainFolder}`;
+const tempDirectory = `${RNFS.CachesDirectoryPath}/TreeMapper_Data_${Date.now()}.zip`;
 
 export const createNewInterventionFolder = async (id: string) => {
     try {
-        const folderPath = `${basePath}/${id}`
-        // Check if the folder already exists
+        const folderPath = `${basePath}/${id}`;
         const folderExists = await RNFS.exists(folderPath);
-
         if (!folderExists) {
-            // Create the folder
             await RNFS.mkdir(folderPath);
         } else {
             console.log('Folder already exists at:', folderPath);
@@ -26,60 +23,54 @@ export const createNewInterventionFolder = async (id: string) => {
 
 const exportRealmData = async (data: InterventionData) => {
     try {
-        await createNewInterventionFolder(data.intervention_id)
+        await createNewInterventionFolder(data.intervention_id);
     } catch (error) {
-        console.log("error", error)
+        console.error("Error exporting realm data:", error);
     }
 }
 
 export const interData = async (data: InterventionData) => {
     try {
-        const folderPath = `${basePath}/${data.intervention_id}/intervention.json`;
+        const filePath = `${basePath}/${data.intervention_id}/intervention.json`;
         const jsonData = JSON.stringify(data);
-        await RNFS.writeFile(folderPath, jsonData, 'utf8');
+        await RNFS.writeFile(filePath, jsonData, 'utf8');
     } catch (error) {
-        console.log("Error", error)
+        console.error("Error writing intervention data:", error);
     }
 }
 
 export const createBasePath = async () => {
-    try { await createNewInterventionFolder(basePath); } catch (err) { console.log("Error", err) }
+    try {
+        const baseFolderExists = await RNFS.exists(basePath);
+        if (!baseFolderExists) {
+            await RNFS.mkdir(basePath);
+        }
+    } catch (error) {
+        console.error("Error creating base path:", error);
+    }
 }
 
-
-export const exportAllInterventionData = async (dataArray: InterventionData[] | any) => {
+export const exportAllInterventionData = async (dataArray: InterventionData[]) => {
     try {
-        const promises = dataArray.map(async (data) => {
-            console.log("Data writing started")
-            await exportRealmData(data);
-        });
+        const createFolderPromises = dataArray.map(data => exportRealmData(data));
+        await Promise.all(createFolderPromises);
 
-        // Wait for all operations to complete
-        await Promise.all(promises);
-        const ss = dataArray.map(async (data) => {
-            console.log("Data writing started")
-            await interData(data);
-        });
-        await Promise.all(ss);
+        const writeDataPromises = dataArray.map(data => interData(data));
+        await Promise.all(writeDataPromises);
 
         console.log("Success");
-        zipAndShareFolder()
+        await zipAndShareFolder();
     } catch (error) {
         console.error("Error processing data:", error);
     }
-};
-
+}
 
 const zipAndShareFolder = async () => {
     try {
-        // Define the path for the zip file
         const zipFilePath = tempDirectory;
-
-        // Zip the folder
         const path = await zip(basePath, zipFilePath);
-        console.log(`zip completed at ${path}`);
+        console.log(`Zip completed at ${path}`);
 
-        // Share the zip file
         const shareOptions = {
             title: 'Share Folder',
             url: `file://${zipFilePath}`,
@@ -87,10 +78,7 @@ const zipAndShareFolder = async () => {
         };
 
         await Share.open(shareOptions);
-
-        // Clean up the zip file if needed
-        // await RNFS.unlink(zipFilePath);
     } catch (error) {
-        console.log('Error zipping and sharing folder:', error);
+        console.error('Error zipping and sharing folder:', error);
     }
 };
