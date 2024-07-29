a7b9132ab796a80916931a54564b760e88e0ef2e
shyambhongle <shyambhongle@gmail.com>
2024-07-29 15:10:43 +0530

    fix: Intervention header not showing list for unsync & incomplete label

--- src/utils/helpers/interventionHelper/groupInterventions.ts
+++ src/utils/helpers/interventionHelper/groupInterventions.ts
@@ -12,11 +12,12 @@ interface FinalObject {
 
 export const groupIntervention = (data: any[] | InterventionData[]) => {
   const finalObject: FinalObject = {};
+  const incompleteKey  = i18next.t('label.incomplete')
   finalObject[i18next.t('label.all')] = {
     count: data.length,
     id: 'all',
   };
-  finalObject[i18next.t('label.incomplete')] = {
+  finalObject[incompleteKey] = {
     count: 0,
     id: 'incomplete',
   };
@@ -25,34 +26,23 @@ export const groupIntervention = (data: any[] | InterventionData[]) => {
     id: 'unsync',
   };
 
-  const key = i18next.t('label.incomplete');
-  if (finalObject[key]) {
-    finalObject[key].count += 1;
-  }
-
   for (const item of data) {
     if (!item.is_complete) {
-      finalObject[i18next.t('label.incomplete')] = {
-        count: finalObject[key]?.count ?? 0,
-        ...finalObject[i18next.t('label.incomplete')],
+      finalObject[incompleteKey] = {
+        count: (finalObject[incompleteKey].count += 1),
+        ...finalObject[incompleteKey],
       };
-    }
-    if (finalObject['Unsynced']) {
-      finalObject['Unsynced'].count += 1;
-    } else {
-      finalObject['Unsynced'] = { count: 1 }; // Initialize if not exists
     }
     if (item.status !== 'SYNCED' && item.is_complete) {
       finalObject['Unsynced'] = {
-        count:  finalObject['Unsynced'].count,
+        count: finalObject['Unsynced'].count += 1,
         ...finalObject['Unsynced'],
       };
     }
     if (finalObject[item.intervention_title]) {
-      finalObject[item.intervention_title].count += 1;
       finalObject[item.intervention_title] = {
+        count: (finalObject[item.intervention_title].count += 1),
         ...finalObject[item.intervention_title],
-        count: finalObject[item.intervention_title].count,
       };
     } else {
       finalObject[item.intervention_title] = {
