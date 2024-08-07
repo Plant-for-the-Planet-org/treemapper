63a3230b40ec15a131942aa001fb9708b2979047
shyambhongle <shyambhongle@gmail.com>
2024-08-07 12:11:01 +0530

    Hide monitoring plot

--- src/screens/PlotView.tsx
+++ src/screens/PlotView.tsx
@@ -1,68 +1,22 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import Header from 'src/components/common/Header'
-import PlotList from 'src/components/monitoringPlot/PlotList'
+import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Colors, Typography } from 'src/utils/constants'
-import { useNavigation } from '@react-navigation/native'
-import { StackNavigationProp } from '@react-navigation/stack'
-import { RootStackParamList } from 'src/types/type/navigation.type'
-import AddIcon from 'assets/images/svg/MoreOptionIcon.svg'
-import { useQuery } from '@realm/react'
-import { RealmSchema } from 'src/types/enum/db.enum'
-import { MonitoringPlot } from 'src/types/interface/slice.interface'
-import Popover from 'react-native-popover-view';
-import i18next from 'src/locales/index'
-import { ctaHaptic } from 'src/utils/helpers/hapticFeedbackHelper'
-
+import i18next from 'i18next'
+import Header from 'src/components/common/Header'
+import EmptyPlotIcon from 'assets/images/svg/EmptyPlotIcon.svg'
 
 const PlotView = () => {
-  const [popupVisible, setPopupVisible] = useState(false)
-  const plotData = useQuery<MonitoringPlot>(
-    RealmSchema.MonitoringPlot,
-    data => {
-      return data.filtered("lastScreen != 'form'")
-    },
-  )
-
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>()
-
-  const addGroups = () => {
-    togglePopup()
-    navigation.navigate('PlotGroup')
-  }
-
-  const togglePopup = () => {
-    ctaHaptic()
-    setPopupVisible(!popupVisible)
-  }
-
-
-
-  const renderIcon = () => {
-    return <Popover
-      isVisible={popupVisible}
-      backgroundStyle={{ opacity: 0 }}
-      popoverStyle={{
-
-      }}
-      onRequestClose={togglePopup}
-      from={(
-        <Pressable onPress={togglePopup} style={styles.rightContainer}><AddIcon width={16} height={16} fill={Colors.WHITE} /></Pressable>
-      )}>
-      <View style={styles.popOverWrapper}>
-        <Pressable onPress={addGroups}><Text style={styles.menuLabel}>{i18next.t('label.plot_group')}</Text></Pressable>
-      </View>
-
-    </Popover>
-
-
-  }
   return (
     <SafeAreaView style={styles.container}>
-      <Header label={i18next.t('label.monitoring_plot_header')} showBackIcon={false} rightComponent={renderIcon()} />
+      <Header label={i18next.t('label.monitoring_plot_header')} showBackIcon={false} />
       <View style={styles.wrapper}>
-        <PlotList data={[...plotData]} />
+        <View style={styles.backDropWrapper}>
+          <EmptyPlotIcon />
+        </View>
+        <Text style={styles.comingSoonLabel}>
+          Coming Soon
+        </Text>
       </View>
     </SafeAreaView>
   )
@@ -73,39 +27,26 @@ export default PlotView
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.WHITE
+    backgroundColor: Colors.WHITE,
   },
   wrapper: {
     flex: 1,
-    backgroundColor: Colors.BACKDROP_COLOR,
-  },
-  rightContainer: {
-    width: 30,
-    height: 30,
     justifyContent: 'center',
     alignItems: 'center',
-    marginRight: 20,
-    borderRadius: 8
+    backgroundColor: Colors.BACKDROP_COLOR,
+    paddingBottom:'20%'
   },
-  popOverWrapper: {
-    width: 100,
-    height: 40,
+  backDropWrapper: {
+    width: '100%',
     justifyContent: 'center',
-    alignItems: "center",
-    borderWidth: 0.5,
-    borderColor: Colors.PALE_WHITE,
-    backgroundColor: Colors.WHITE,
-    shadowColor: Colors.PALE_WHITE,
-    shadowOffset: { width: 2, height: 2 },
-    shadowOpacity: 0.6,
-    shadowRadius: 5,
-    elevation: 2,
-    borderRadius: 8,
+    alignItems: 'center'
   },
-  menuLabel: {
-    fontSize: 14,
-    fontFamily: Typography.FONT_FAMILY_SEMI_BOLD,
-    color: Colors.TEXT_COLOR,
-
+  comingSoonLabel: {
+    width: '90%',
+    textAlign: 'center',
+    marginTop: 10,
+    color:Colors.TEXT_LIGHT,
+    fontSize:20,
+    fontFamily:Typography.FONT_FAMILY_SEMI_BOLD
   }
 })
