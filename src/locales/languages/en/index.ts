4ff5bf43dc4d717ce3ac89af982155b66a4dac44
shyambhongle <shyambhongle@gmail.com>
2024-08-17 10:38:21 +0530

    minor

--- src/locales/languages/en/index.ts
+++ src/locales/languages/en/index.ts
@@ -1,34 +1,49 @@
-// import SignUp from './authentication/signUplabels.json';
-// import InitialScreen from './static/initialScreen.json';
-// import Permission from './static/permission.json';
-// import WelcomeScreen from './static/welcomeScreen.json';
-// import DownloadMap from './user/downloadMap.json';
-// import EditUserProfile from './user/editUserProfilelabels.json';
-// import InventoryOverview from './user/inventoryOverview.json';
-// import LocateTree from './user/locateTree.json';
-// import Logs from './user/logs.json';
-// import ManageUser from './user/manageUser.json';
-// import RegisterTree from './user/registerTreeslabels.json';
-// import SavedAreas from './user/savedAreas.json';
-// import SelectSpecies from './user/selectSpecies.json';
-// import MapMarking from './user/mapMarking.json';
-// import TreeInventory from './user/treeInventory.json';
-// import Review from './user/treeReview.json';
-// import SampleTrees from './user/sampleTrees.json';
-// import SpeciesSyncError from './user/speciesSyncError.json';
-// import AdditionalData from './user/additionalData.json';
-// import Projects from './user/projects.json';
-// import Intervention from './user/intervention.json'
-// import MonitoringPlot from './user/monitoringPlot.json'
-// import CommonStrings from './user/commonData.json'
-
-
-import Species from './species.json'
-
+import SignUp from './authentication/signUplabels.json';
+import InitialScreen from './static/initialScreen.json';
+import Permission from './static/permission.json';
+import WelcomeScreen from './static/welcomeScreen.json';
+import DownloadMap from './user/downloadMap.json';
+import EditUserProfile from './user/editUserProfilelabels.json';
+import InventoryOverview from './user/inventoryOverview.json';
+import LocateTree from './user/locateTree.json';
+import Logs from './user/logs.json';
+import ManageUser from './user/manageUser.json';
+import RegisterTree from './user/registerTreeslabels.json';
+import SavedAreas from './user/savedAreas.json';
+import SelectSpecies from './user/selectSpecies.json';
+import MapMarking from './user/mapMarking.json';
+import TreeInventory from './user/treeInventory.json';
+import Review from './user/treeReview.json';
+import SampleTrees from './user/sampleTrees.json';
+import SpeciesSyncError from './user/speciesSyncError.json';
+import AdditionalData from './user/additionalData.json';
+import Projects from './user/projects.json';
+import Intervention from './user/intervention.json'
+import MonitoringPlot from './user/monitoringPlot.json'
+import CommonStrings from './user/commonData.json'
 
 export default {
-  "species": Species,
-  "intervention":{},
-  "common":{},
-  "form":{},
+  ...WelcomeScreen,
+  ...SignUp,
+  ...RegisterTree,
+  ...LocateTree,
+  ...TreeInventory,
+  ...EditUserProfile,
+  ...InventoryOverview,
+  ...DownloadMap,
+  ...ManageUser,
+  ...SavedAreas,
+  ...Permission,
+  ...Review,
+  ...SelectSpecies,
+  ...InitialScreen,
+  ...Logs,
+  ...SampleTrees,
+  ...MapMarking,
+  ...SpeciesSyncError,
+  ...AdditionalData,
+  ...Projects,
+  ...Intervention,
+  ...MonitoringPlot,
+  ...CommonStrings
 };
