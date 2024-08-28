136fdeb205fc69f27921eebf8b82b0b04cbd9cdd
shyambhongle <shyambhongle@gmail.com>
2024-08-28 14:29:04 +0530

    Monitoring Plot Vector added

--- src/screens/PlotView.tsx
+++ src/screens/PlotView.tsx
@@ -1,15 +1,20 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from 'src/components/common/Header'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Colors, Typography } from 'src/utils/constants'
 import i18next from 'src/locales/index'
-
+import ComingSoon from 'assets/images/svg/ComingSoon.svg'
 
 const PlotView = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header label={i18next.t('label.monitoring_plot_header')} showBackIcon={false} />
+      <View style={styles.wrapper}>
+        <ComingSoon />
+        <Text style={styles.mainLabel}>Monitoring Plots Coming Soon</Text>
+        <Text style={styles.secondaryLabel}>You'll soon be able to monitor and {'\n'}manage your plots with detailed insights.{'\n'}Stay tuned!</Text>
+      </View>
     </SafeAreaView>
   )
 }
@@ -24,34 +29,23 @@ const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     backgroundColor: Colors.BACKDROP_COLOR,
+    justifyContent:'center',
+    alignItems:'center'
   },
-  rightContainer: {
-    width: 30,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginRight: 20,
-    borderRadius: 8
+  mainLabel: {
+    fontSize: 18,
+    color: Colors.DARK_TEXT,
+    fontFamily: Typography.FONT_FAMILY_BOLD,
+    width:'100%',
+    textAlign:'center',
+    marginTop:10
   },
-  popOverWrapper: {
-    width: 100,
-    height: 40,
-    justifyContent: 'center',
-    alignItems: "center",
-    borderWidth: 0.5,
-    borderColor: Colors.PALE_WHITE,
-    backgroundColor: Colors.WHITE,
-    shadowColor: Colors.PALE_WHITE,
-    shadowOffset: { width: 2, height: 2 },
-    shadowOpacity: 0.6,
-    shadowRadius: 5,
-    elevation: 2,
-    borderRadius: 8,
-  },
-  menuLabel: {
-    fontSize: 14,
-    fontFamily: Typography.FONT_FAMILY_SEMI_BOLD,
+  secondaryLabel: {
+    fontSize: 16,
     color: Colors.TEXT_COLOR,
-
-  }
+    fontFamily: Typography.FONT_FAMILY_REGULAR,
+    marginTop: 10,
+    width:'100%',
+    textAlign:'center',
+  },
 })
