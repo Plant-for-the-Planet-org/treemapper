9d440f6b9ca425f2891166c1092e461ce14a31ec
shyambhongle <shyambhongle@gmail.com>
2024-09-03 22:46:31 +0530

    Swipable added

--- src/components/intervention/InterventionCard.tsx
+++ src/components/intervention/InterventionCard.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { scaleFont } from 'src/utils/constants/mixins'
 import InterventionIconSwitch from './InterventionIconSwitch'
@@ -12,11 +12,21 @@ import InterventionCardHeader from './InterventionCardHeader'
 import InterventionMetaInfo from './InterventionMetaInfo'
 import DividerDot from '../common/DividerDot'
 import i18next from 'i18next'
+import SwipeableItem from "react-native-swipeable-item";
+
+
 
 
 interface Props {
   item: InterventionData
   openIntervention: (item: InterventionData) => void
+}
+const OVERSWIPE_DIST = 20;
+
+const swipeableLeftComp = () => {
+  return <View style={styles.leftContainer}><View style={styles.leftWrapper}>
+    <Text>Left</Text>
+  </View></View>
 }
 
 const InterventionCard = (props: Props) => {
@@ -25,51 +35,61 @@ const InterventionCard = (props: Props) => {
     const finalItem = { ...JSON.parse(JSON.stringify(item)) }
     openIntervention({ ...finalItem })
   }
-  const renderFixLabel=()=>{
+  const renderFixLabel = () => {
     switch (item.fix_required) {
       case "PROJECT_ID_MISSING":
         return "Project not assigned"
       default:
-      return "Fix Require"      
+        return "Fix Require"
     }
   }
   return (
-    <TouchableOpacity style={styles.container} onPress={handleIntervention}>
-      <View style={styles.wrapper}>
-        <InterventionIconSwitch icon={item.intervention_type} />
-        {!item.is_complete && item.fix_required === "NO"? (
-          <View style={styles.incompleteTagWrapper}>
-            <Text style={styles.incompleteTagLabel}>{i18next.t("label.incomplete")}</Text>
-          </View>
-        ) : null}
-        {item.is_complete && item.fix_required !== "NO" ? (
-          <View style={styles.projectAssignWrapper}>
-            <Text style={styles.projectLabel}>{renderFixLabel()}</Text>
-          </View>
-        ) : null}
-        <View style={styles.sectionWrapper}>
-          <InterventionCardHeader item={item} />
-          <View style={styles.metaWrapper}>
-            <View style={styles.syncIconWrapper}>
-              {item.status !== "SYNCED" ? (
-                <UnSyncIcon width={15} height={15} />
-              ) : (
-                <SyncIcon width={15} height={15} />
-              )}
+    <SwipeableItem
+      key={item.intervention_id}
+      item={item}
+      overSwipe={OVERSWIPE_DIST}
+      renderUnderlayLeft={() => (swipeableLeftComp())}
+      renderUnderlayRight={() => <Text>Right</Text>}
+      snapPointsLeft={[175]}
+      snapPointsRight={[0]}
+    >
+      <Pressable style={styles.container} onPress={handleIntervention}>
+        <View style={styles.wrapper}>
+          <InterventionIconSwitch icon={item.intervention_type} />
+          {!item.is_complete && item.fix_required === "NO" ? (
+            <View style={styles.incompleteTagWrapper}>
+              <Text style={styles.incompleteTagLabel}>{i18next.t("label.incomplete")}</Text>
             </View>
-            <Text style={styles.metaLabel}>{item.location.type}</Text>
-            <DividerDot width={20} height={20} size={20} color={Colors.TEXT_COLOR} />
-            <InterventionMetaInfo item={item} />
-            <Text style={styles.metaLabel}>
-              {timestampToBasicDate(item.intervention_date)}
-            </Text>
+          ) : null}
+          {item.is_complete && item.fix_required !== "NO" ? (
+            <View style={styles.projectAssignWrapper}>
+              <Text style={styles.projectLabel}>{renderFixLabel()}</Text>
+            </View>
+          ) : null}
+          <View style={styles.sectionWrapper}>
+            <InterventionCardHeader item={item} />
+            <View style={styles.metaWrapper}>
+              <View style={styles.syncIconWrapper}>
+                {item.status !== "SYNCED" ? (
+                  <UnSyncIcon width={15} height={15} />
+                ) : (
+                  <SyncIcon width={15} height={15} />
+                )}
+              </View>
+              <Text style={styles.metaLabel}>{item.location.type}</Text>
+              <DividerDot width={20} height={20} size={20} color={Colors.TEXT_COLOR} />
+              <InterventionMetaInfo item={item} />
+              <Text style={styles.metaLabel}>
+                {timestampToBasicDate(item.intervention_date)}
+              </Text>
+            </View>
           </View>
+          {item.status !== 'SYNCED' && <View style={styles.editIconWrapper}>
+            <EditInterventionIcon height={30} width={30} />
+          </View>}
         </View>
-        {item.status !== 'SYNCED' && <View style={styles.editIconWrapper}>
-          <EditInterventionIcon height={30} width={30} />
-        </View>}
-      </View>
-    </TouchableOpacity>
+      </Pressable>
+    </SwipeableItem>
   )
 }
 
@@ -99,37 +119,37 @@ const styles = StyleSheet.create({
   incompleteTagWrapper: {
     justifyContent: 'center',
     alignItems: 'center',
-    height:18,
+    height: 18,
     backgroundColor: Colors.GRAY_BACKDROP,
     position: 'absolute',
     top: 0,
-    width:'22%',
+    width: '22%',
     right: 10,
     borderBottomRightRadius: 10,
     borderBottomLeftRadius: 10,
-    paddingHorizontal:5
+    paddingHorizontal: 5
   },
   incompleteTagLabel: {
-    fontSize:8,
+    fontSize: 8,
     fontFamily: Typography.FONT_FAMILY_SEMI_BOLD,
   },
   projectAssignWrapper: {
     justifyContent: 'center',
     alignItems: 'center',
-    height:18,
-    width:'25%',
+    height: 18,
+    width: '25%',
     backgroundColor: '#ff8383',
     position: 'absolute',
     top: 0,
     right: 10,
     borderBottomRightRadius: 10,
     borderBottomLeftRadius: 10,
-    paddingHorizontal:5
+    paddingHorizontal: 5
   },
   projectLabel: {
-    fontSize:8,
+    fontSize: 8,
     fontFamily: Typography.FONT_FAMILY_SEMI_BOLD,
-    color:Colors.WHITE
+    color: Colors.WHITE
   },
   editIconWrapper: {
     height: '100%',
@@ -161,5 +181,25 @@ const styles = StyleSheet.create({
   metaLabel: {
     fontSize: scaleFont(12),
     fontFamily: Typography.FONT_FAMILY_REGULAR,
+  },
+  leftContainer: {
+    width: '100%',
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  leftWrapper: {
+    width: '90%',
+    height: '80%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 12,
+    backgroundColor: Colors.WHITE,
+    elevation: 5, // This adds a shadow on Android
+    shadowColor: Colors.GRAY_TEXT,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    paddingLeft: 10,
   },
 })
