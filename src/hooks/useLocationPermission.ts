0a9f41e77dbebc82b35f193aeb7a92e804cc10ab
shyambhongle <shyambhongle@gmail.com>
2024-12-30 13:55:14 +0530

    Removed Unwanted GPS call

--- src/hooks/useLocationPermission.ts
+++ src/hooks/useLocationPermission.ts
@@ -67,7 +67,6 @@ const useLocationPermission = () => {
 
 
   const requestLocationPermission = async () => {
-    await Location.enableNetworkProviderAsync()
     await requestForegroundPermissionsAsync()
   }
 
