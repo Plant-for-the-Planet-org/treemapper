d998edf8d1ebdcdab44d2a0ef530fa3edb29c6d0
shyambhongle <shyambhongle@gmail.com>
2025-01-20 22:36:11 +0530

    clean

--- src/db/utils.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Converts Realm Object(immutable, outside write transaction) into a javascript object(mutable everywhere)
- * @param  {any|Realm.Object} object
- */
-export const getJSONFromRealmObject = (object: Realm.Object) =>object.toJSON();
--- src/utils/helpers/timeHelper.ts
+++
@@ -1,7 +0,0 @@
-export const isWithin90Days = (timestamp: number) => {
-  const timestampDate = new Date(timestamp)
-  const ninetyDaysAgo = new Date(
-    timestampDate.getTime() - 90 * 24 * 60 * 60 * 1000,
-  )
-  return timestampDate >= ninetyDaysAgo
-}
