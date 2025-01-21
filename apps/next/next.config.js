d37265a2f4c62e9b432f5f77b43b7b48c8dc1fc4
shyambhongle <shyambhongle@gmail.com>
2025-01-21 09:49:22 +0530

    config files for solito

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -2,26 +2,23 @@ const { withExpo } = require('@expo/next-adapter')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // React Native Web config
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      'react-native$': 'react-native-web',
-    }
-    config.resolve.extensions = [
-      '.web.js',
-      '.web.jsx',
-      '.web.ts',
-      '.web.tsx',
-      ...config.resolve.extensions,
-    ]
-    return config
-  },
+  // reanimated (and thus, Moti) doesn't work with strict mode currently...
+  // https://github.com/nandorojo/moti/issues/224
+  // https://github.com/necolas/react-native-web/pull/2330
+  // https://github.com/nandorojo/moti/issues/224
+  // once that gets fixed, set this back to true
+  reactStrictMode: false,
   transpilePackages: [
     'react-native',
     'react-native-web',
     'solito',
-    '@treemapper/app',
+    'dripsy',
+    '@dripsy/core',
+    'moti',
+    'app',
+    'react-native-reanimated',
+    '@expo/html-elements',
+    'react-native-gesture-handler',
   ],
 }
 
