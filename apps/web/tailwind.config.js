1b303098da23e0ba8b1fa0044cac2f56ee60bea4
shyambhongle <shyambhongle@gmail.com>
2025-04-18 12:39:55 +0530

    tailwind shared package fix

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: [
       './app/**/*.{js,ts,jsx,tsx,mdx}',
       './components/**/*.{js,ts,jsx,tsx,mdx}',
+       "../../packages/dashboard/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
       extend: {},
