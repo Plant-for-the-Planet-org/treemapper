c5c014ddf13abe24040366b39f1d9e5e671a9e46
shyambhongle <shyambhongle@gmail.com>
2025-07-01 16:10:21 +0530

    web cleanup

--- apps/web/app/dashboard/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import Settings from 'dashboard/pages/settings/SettingsHome'
-export default function Dashboard() {
-    return (
-        <div className='w-full h-full'>
-            <Settings />
-        </div>
-    );
-}
--- apps/web/app/providers/Auth0Provider.tsx
+++
@@ -1,9 +0,0 @@
-// app/providers/Auth0Provider.tsx
-'use client';
-
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-import React from 'react';
-
-export default function Auth0Provider({ children }: { children: React.ReactNode }) {
-  return <UserProvider>{children}</UserProvider>;
-}
