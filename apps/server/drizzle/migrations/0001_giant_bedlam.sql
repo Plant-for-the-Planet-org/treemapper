461fa6d6f33ffad0c90311a67e8e0f54987a4493
shyambhongle <shyambhongle@gmail.com>
2025-08-05 14:04:27 +0530

    server update

--- apps/server/drizzle/migrations/0001_giant_bedlam.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "species_request" DROP CONSTRAINT "valid_urgency";--> statement-breakpoint
-ALTER TABLE "species_request" DROP CONSTRAINT "reviewed_status_has_reviewer";--> statement-breakpoint
-ALTER TABLE "species_request" DROP CONSTRAINT "rejected_has_reason";--> statement-breakpoint
-ALTER TABLE "species_request" DROP CONSTRAINT "approved_has_species";--> statement-breakpoint
-ALTER TABLE "species_request" DROP CONSTRAINT "duplicate_has_reference";--> statement-breakpoint
-ALTER TABLE "species_request" DROP CONSTRAINT "reviewed_at_after_created";--> statement-breakpoint
-ALTER TABLE "species_request" DROP CONSTRAINT "scientific_name_format";
