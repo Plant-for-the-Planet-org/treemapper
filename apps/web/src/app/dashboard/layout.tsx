4a659886d9ab606f6ebdd3c74254809c0ea75d78
shyambhongle <shyambhongle@gmail.com>
2026-02-23 15:21:49 +0530

    force-dynamic key added

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import MaintenanceScreen from '@/component/MaintenanceScreen';
 import DashboardClientLayout from './DashboardClientLayout';
 
+export const dynamic = 'force-dynamic';
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   if (process.env.MAINTENANCE_MODE === 'true') {
     return <MaintenanceScreen restoreTime={process.env.MAINTENANCE_RESTORE_TIME} />;
